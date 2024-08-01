b77d4e4a0f18c3ab9727f40bc24321318fdb54eb
Jânio <janioofi@gmail.com>
2024-08-01 14:04:18 -0300

    feat: Criado um record UsuarioResponseDto.

--- BackEnd/src/main/java/com/codigocerto/backend/domain/dtos/UsuarioResponseDto.java
+++ BackEnd/src/main/java/com/codigocerto/backend/domain/dtos/UsuarioResponseDto.java
@@ -1,4 +1,21 @@
 package com.codigocerto.backend.domain.dtos;
 
-public record UsuarioResponseDto() {
+import com.codigocerto.backend.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record UsuarioResponseDto(
+
+        Long idUsuario,
+        String nome,
+        String sobrenome,
+        String email,
+        String disponibilidade,
+        String area,
+        String linguagem,
+        String descricao,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime dataCadastro,
+        Status status
+) {
 }
