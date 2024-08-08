be1d11acf501dec548d77027927bd016ef273ad2
Douglas Candido <doug.candido2704@gmail.com>
2024-08-08 05:57:33 -0300

    feat: adicionados métodos createUsuario e findAll na classe UsuarioController.

--- BackEnd/src/main/java/com/codigocerto/backend/controllers/UsuarioController.java
+++ BackEnd/src/main/java/com/codigocerto/backend/controllers/UsuarioController.java
@@ -1,4 +1,37 @@
 package com.codigocerto.backend.controllers;
 
+
+import com.codigocerto.backend.domain.dtos.UsuarioRequestDto;
+import com.codigocerto.backend.domain.dtos.UsuarioResponseDto;
+import com.codigocerto.backend.domain.entities.Usuario;
+import com.codigocerto.backend.domain.services.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDto> createUsuario(@Valid @RequestBody UsuarioRequestDto usuarioRequestDto) {
+        UsuarioResponseDto usuarioResponseDto = usuarioService.create(usuarioRequestDto);
+        return new ResponseEntity<>(usuarioResponseDto, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public  ResponseEntity<List<UsuarioResponseDto>> findAll(){
+        List<UsuarioResponseDto> usuarioResponseDtos = usuarioService.findAll();
+        return new ResponseEntity<>(usuarioResponseDtos, HttpStatus.OK);
+            }
 }
